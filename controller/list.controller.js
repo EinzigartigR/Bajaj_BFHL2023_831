288d73298283b3f6942440d5b5df26102421f63a
EinzigartigR <ana392rana@gmail.com>
2022-12-29 23:10:38 +0530

    Feature/AR/GB-3 err handling in list controller

--- controller/list.controller.js
+++ controller/list.controller.js
@@ -1,46 +1,58 @@
-const User = require('../models/user.model');
 const List = require('../models/list.model');
 require('dotenv').config();
 
 
-exports.setItems = async function(req,res,next){
-    const data = await List.create({
+exports.setItems = async function(req,res){
+    const list = await List.create({
         taskName : req.body.taskName,
         status : req.body.status,
         username : req.body.username,
+    }, (err)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send({message:"item has been added to the list"})   
     })
-    res.status(200).send({message:"item has been added to the list"})
 }
 
-exports.getListItems = async function(req,res,next){
+exports.getListItems = async function(req,res){
     const data = await List.find().sort({
         createdAt : -1
+    }, (err,list) => {
+        if(err){
+            res.status(500).send(err);
+        }
+        if(!list){
+            res.status(401).send({message:"No list has been made"});
+        }
+
+        res.send(data);
     });
-    res.send(data);
+
 }
 
 exports.deleteItem = async function(req,res){
-    const data = await List.findOneAndDelete({username:req.body.username, taskName:req.body.taskName}).sort({createdAt: -1});
-    res.status(200).send({message:"Item has been Removed"});
+    const data = await List.findOneAndDelete({username:req.body.username, taskName:req.body.taskName}, (err,user)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        if(!user){
+            res.status(401).send({message:"User with this task does not exist"});
+        }
+        res.status(200).send({message:"Item has been Removed"});
+    })
 }
 
 exports.updateItem = function(req,res){
-    User.findOne({
-        username:req.body.username
-    }).exec((err,user) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-        if(!user){
-            res.status(404).send({ message: 'User Not Found' });
-            return;
-        }
-    
-        if(user.taskName == req.body.taskName){
-           List.findOneAndUpdate({status: req.body.status}).sort({createdAt: -1});
-            res.status(200).send({message:user.taskName + "'s Status has been Updated to " + user.status});
-        }
-    })
+ List.findOneAndUpdate({username: req.body.username , taskName: req.body.taskName},{$set : {status:req.body.status}}, (err,user) => {
+    if(err){
+        res.status(500).send(err);
+    }
+
+    if(!user){
+        res.status(401).send({message:"User with this task does not exist"});
+    }
+    res.status(200).send({message:req.body.taskName + " has been Updated"});
+ })
 
 }
