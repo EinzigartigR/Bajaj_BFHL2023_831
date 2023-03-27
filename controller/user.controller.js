265897b35461c3cda0f2c49498435fa7e8b8e5ac
Diwakar Adhikari <diwakar.adhikari@bajajfinserv.in>
2023-03-27 13:15:30 +0530

    added OTP generate and verify

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,28 +1,72 @@
 const User = require('../models/user.model');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
+var otp = '';
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
 require('dotenv').config();
 
-exports.signUp = async function(req,res){
+exports.signUp = async function (req, res) {
     const user = await new User({
-        username : req.body.username,
-        email : req.body.email,
-        password : bcrypt.hashSync(req.body.password,8),
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
     })
     user.save(err => {
         if (err) {
-          res.status(500).send({ message: err });
+            res.status(500).send({ message: err });
         }
         res.send({ message: "User was registered successfully!" });
-      });
+    });
 }
 
-exports.signIn = (req,res) => {
-    var token = jwt.sign({id:req.body.username},process.env.secret,{
-        expiresIn:86400 //24hr
+exports.signIn = (req, res) => {
+    var token = jwt.sign({ id: req.body.username }, process.env.secret, {
+        expiresIn: 86400 //24hr
     })
     res.status(200).send({
-        user:req.body.username, access_token:token
+        user: req.body.username, access_token: token
     });
 
+}
+
+function OTP() {
+    const digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
+
+exports.generateOTP = (req, res) => {
+    otp = OTP();
+    client.messages
+        .create({
+            body: 'Your OTP for verification is : ' + otp,
+            from: process.env.twilioNumber, // Twilio number
+            to: req.body.phoneNumber // Mobile number
+        })
+        .then(message =>
+            res.status(200).send({
+                message
+            })
+        );
+}
+
+exports.validateOTP = (req, res) => {
+    console.log(otp);
+    console.log(req.body)
+    if (otp == 'undefined' || otp === null || otp == '') {
+        code = 401;
+        message = "Please call the generateOTP API first"
+    }
+    else if (req.body.otp == otp && !otp) {
+        code = 200;
+        message = "User mobile has been verified"
+    }
+    else {
+        code = 400;
+        message = "You may have entered the wrong OTP"
+    }
+    res.status(code).send(message);
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,12 +4,16 @@ const verifySignUp = require('../middlewares/verifySignUp');
 const verifySignIn = require('../middlewares/verifySignIn');
 var router = express.Router();
 
-router.get('/', function (req, res) { 
+router.get('/', function (req, res) {
     res.send('user Works');
- });
+});
 
 router.post('/signUp', [verifySignUp.checkDuplicateUsernameOrEmail], controller.signUp); //first goes to middleware and verifies, then to the controller
 
-router.post('/signIn', [verifySignIn.checkUserAndPassword] , controller.signIn);
+router.post('/signIn', [verifySignIn.checkUserAndPassword], controller.signIn);
+
+router.post('/generateOTP', controller.generateOTP)
+
+router.post('/validateOTP', controller.validateOTP)
 
 module.exports = router;
